ac8afb7dad7c75704dfa883fe2d6a64130dc267a
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-08-24 21:36:51 -0400

    ADDED get newGame

--- Program.cs
+++ Program.cs
@@ -3,14 +3,16 @@ using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-  c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minesweeper API", Description = "An ASP.NET API built with Microsoft SQL Server for running minesweeper", Version = "v1"});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minesweeper API", Description = "An ASP.NET API built with Microsoft SQL Server for running minesweeper", Version = "v1" });
 });
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI(c => {
-  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minesweeper API V1");
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minesweeper API V1");
 });
 
 app.UseHttpsRedirection();
@@ -19,6 +21,30 @@ string connectionString = app.Configuration.GetConnectionString("AZURE_SQL_CONNE
 
 // string tableQuery = "CREATE TABLE MinesweeperGames (id uniqueidentifier NOT NULL, board char(256) NOT NULL, timestart bigint, timeend bigint, completed bit);";
 
+static string GetNewBoard()
+{
+    // create a string to insert as the new board.
+    return "";
+}
 
+app.MapGet("/newGame", () => {
+    using var conn = new SqlConnection(connectionString);
+    conn.Open();
+
+    Guid uuid = Guid.NewGuid();
+
+    // create a new game with a post query
+    var command = new SqlCommand(
+        "INSERT INTO MinesweeperGames (id, board) VALUES (@id, @board)", conn
+    );
+    command.Parameters.Clear();
+    command.Parameters.AddWithValue("id", uuid);
+    command.Parameters.AddWithValue("@board", GetNewBoard());
+
+    using SqlDataReader reader = command.ExecuteReader();
+
+    return uuid;
+})
+.WithName("New Game");
 
 app.Run();
